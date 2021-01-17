92512644bb66dfa44c424076d1cc7d9c07de7b4a
bciyhgj <jihanjia200831@126.com>
2021-01-17 18:41:13 +0800

    添加微信事件处理

--- app/Http/Controllers/WeChat/WeChatController.php
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -16,10 +16,10 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app    = app('wechat.official_account');
-        $app->server->push(function ($message) {
-            return "欢迎关注 overtrue！";
-        });
+        // $app    = app('wechat.official_account');
+        // $app->server->push(function ($message) {
+        //     return "欢迎关注 overtrue！";
+        // });
 
         //从项目实例中得到一个服务端应用实例
         // $server = $app->server;
