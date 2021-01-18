c29fde148d1df88a89cf9584bf8372f9ea46a942
bciyhgj <jihanjia200831@126.com>
2021-01-18 15:21:55 +0800

    开放平台全网发布

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 // 微信
 Route::any('/wechat', 'WeChat\WeChatController@serve');
 
+// 拿到用户授权信息
 Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
     Route::get('/user', function () {
         $user = session('wechat.oauth_user'); // 拿到授权用户资料
@@ -25,3 +26,7 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
         dd($user);
     });
 });
+
+// $app_id 是微信传过来的公众号或小程序的APPID
+// 接收微信推送来的事件和消息
+Route::any('receiver/{appid}', 'WeChat\WeChatController@receiver');
