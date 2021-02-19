f7406300aab5ef1e127ecb6075ef3237003d8b72
bciyhgj <jihanjia200831@126.com>
2021-02-19 11:08:21 +0800

    用户信息格式

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,41 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
         $user = session('wechat.oauth_user'); // 拿到授权用户资料
 
         dd($user);
+
+		// array:1 [▼
+		//   "default" => User {#394 ▼
+		//     #attributes: array:8 [▼
+		//       "id" => "oheQ-s2R2SjoSom6oWxLOcnKPGR8"
+		//       "name" => "所有的伟大 源于一个勇敢的开始"
+		//       "nickname" => "所有的伟大 源于一个勇敢的开始"
+		//       "avatar" => "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJibCJxeF0ibxdCjnWic5yhyBfpE8ERZXr8GPXrKVqyftkBcjbPen2X35SbPcFEPMseQC5wQop0NwAew/132"
+		//       "email" => null
+		//       "original" => array:9 [▼
+		//         "openid" => "oheQ-s2R2SjoSom6oWxLOcnKPGR8"
+		//         "nickname" => "所有的伟大 源于一个勇敢的开始"
+		//         "sex" => 1
+		//         "language" => "zh_CN"
+		//         "city" => "杭州"
+		//         "province" => "浙江"
+		//         "country" => "中国"
+		//         "headimgurl" => "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJibCJxeF0ibxdCjnWic5yhyBfpE8ERZXr8GPXrKVqyftkBcjbPen2X35SbPcFEPMseQC5wQop0NwAew/132"
+		//         "privilege" => []
+		//       ]
+		//       "token" => AccessToken {#395 ▼
+		//         #attributes: array:5 [▼
+		//           "access_token" => "9_go7Ks5AmSHgN4j5_vaSvZOwehVuLfyhsVYKpvPEOkPbGjUaL6MUoy0XRdwDTKtwnVoMfB37N10nc05vLB0thGQ"
+		//           "expires_in" => 7200
+		//           "refresh_token" => "9_f8cEfPuPpcRzUib0y14gbGpdLi282oRdsEASstjtv3pNULoAA5khi6ZcxofN2O5jrrfCafUkcjr2jbq758qnyw"
+		//           "openid" => "oheQ-s2R2SjoSom6oWxLOcnKPGR8"
+		//           "scope" => "snsapi_userinfo"
+		//         ]
+		//       }
+		//       "provider" => "WeChat"
+		//     ]
+		//   }
+		// ]
+
+
     });
 });
 
