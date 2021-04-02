d90205b3ec5399a8e2cc839caadc609d5fa071a9
bciyhgj <jihanjia200831@126.com>
2021-04-02 17:03:53 +0800

    二维码界面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>扫码</title>
+
+        <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -91,5 +93,50 @@
                 </div>
             </div>
         </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-xs-offset-3">
+                    <img id="qrcode" src="" class="img-responsive">
+                </div>
+                <div class="col-xs-6 col-xs-offset-3">
+                    <div class="alert alert-warning" role="alert">
+                        <strong>Note!</strong> 扫描后需要关注公众平台测试号，可以过后自行取消关注。
+                    </div>
+                </div>
+                <div class="col-xs-6 col-xs-offset-3">
+                    <div class="alert alert-info hide" role="alert" id="connect-info">
+                        <strong>Well done!</strong> 连接成功.
+                    </div>
+                </div>
+                <div class="col-xs-6 col-xs-offset-3">
+                    <div class="alert alert-success hide" role="alert" id="scan-info">
+                        <strong id="username"></strong> 登录成功. 欢迎关注我的公众号～
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script>
+            const socket = new WebSocket("{{ config('swoole-wechat.ws') }}");
+            socket.addEventListener('open', function (event) {
+                $('#connect-info').removeClass('hide');
+            });
+            socket.addEventListener('message', function (event) {
+                var data = JSON.parse(event.data);
+                console.log(data);
+                if (data.message_type == 'qrcode_url'){
+                    $('#qrcode').attr('src', data.url);
+                }
+                if (data.message_type == 'scan_success'){
+                    $('#scan-info').removeClass('hide');
+                    $('#username').text(data.user);
+                    $('#qrcode').attr('src', '/images/qrcode_for_gh_72b6109b62cf_430.jpg');
+                }
+            });
+        </script>
+
     </body>
 </html>
